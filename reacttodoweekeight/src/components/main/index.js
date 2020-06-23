181097681cb37a828dbe4e41653d3eb07d2d2301
cnickels21 <cnickels_21@yahoo.com>
2020-06-22 23:03:31 -0500

    stuck on getting my form to submit and render in the list

--- reacttodoweekeight/src/components/main/index.js
+++ reacttodoweekeight/src/components/main/index.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 export default function Main(props) {
     const { taskCount = 0 } = props;
-    const { taskTitle = "" } = props;
 
     let [count, setCount] = useState(taskCount);
-    let [title, setTitle] = useState(taskTitle);
+    let [title, setTitle] = useState("");
     let [tasks, setTasks] = useState([]);
-    let [assignee, setAssignee] = useState('');
+    // Not using this yet
+    // let [assignee, setAssignee] = useState('');
 
-    const updateTitle = e => setTitle(e.target.title);
+    const updateTitle = e => setTitle(e.target.value);
 
     const saveTask = e => {
         e.preventDefault();
@@ -26,24 +26,24 @@ export default function Main(props) {
             <form>
                 <h3>Add to your list!</h3>
                 <div id="task-item">
-                    <label for="task">What's your task? </label>
+                    <label>What's your task? </label>
                     <input required type="text" id="task" onChange={updateTitle}></input>
                 </div>
                 <div id="assigned-to">
-                    <label for="assignee">Who's responsibility? </label>
+                    <label>Who's responsibility? </label>
                     <input required type="text" id="assignee" ></input>
                 </div>
                 <fieldset>
                     <legend>Difficulty</legend>
-                    <label for="easier">1</label>
-                    <input type="radio" name="difficulty" value="1" id="easier" required/>
-                    <label for="easy">2</label>
+                    <label>1</label>
+                    <input type="radio" name="difficulty" value="1" id="easier" />
+                    <label>2</label>
                     <input type="radio" name="difficulty" value="2" id="easy" />
-                    <label for="med">3</label>
+                    <label>3</label>
                     <input type="radio" name="difficulty" value="3" id="med" />
-                    <label for="hard">4</label>
+                    <label>4</label>
                     <input type="radio" name="difficulty" value="4" id="hard" />
-                    <label for="harder">5</label>
+                    <label>5</label>
                     <input type="radio" name="difficulty" value="5" id="harder" />
                 </fieldset>
                 <button type="submit" onSubmit={saveTask}>Add Task</button>
